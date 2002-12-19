9f32da41c322bb8943ae37e3adb9547a3520b87f
mostang.com!davidm <mostang.com!davidm>
2002-12-19 07:16:50 +0000

    Initial revision

--- src/x86/Lis_signal_frame-x86.c
+++
@@ -1,4 +0,0 @@
-#ifndef UNW_REMOTE_ONLY
-#define UNW_LOCAL_ONLY
-#include "Gis_signal_frame-x86.c"
-#endif
--- src/x86/Lstep-x86.c
+++
@@ -1,4 +0,0 @@
-#ifndef UNW_REMOTE_ONLY
-#define UNW_LOCAL_ONLY
-#include "Gstep-x86.c"
-#endif
