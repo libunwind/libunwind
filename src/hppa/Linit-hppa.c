8ec7b349f1b9c60e6657da069648a5205c85635b
mostang.com!davidm <mostang.com!davidm>
2003-01-14 07:11:56 +0000

    Initial revision

--- src/hppa/Linit-hppa.c
+++
@@ -1,4 +0,0 @@
-#ifndef UNW_REMOTE_ONLY
-#define UNW_LOCAL_ONLY
-#include "Ginit-hppa.c"
-#endif
--- src/hppa/Linit_local-hppa.c
+++
@@ -1,4 +0,0 @@
-#ifndef UNW_REMOTE_ONLY
-#define UNW_LOCAL_ONLY
-#include "Ginit_local-hppa.c"
-#endif
