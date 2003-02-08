42cfe4f411ad1f33dbb51fbc99e433db99143ac4
mostang.com!davidm <mostang.com!davidm>
2003-02-08 10:10:59 +0000

    (unw_get_proc_name): Add "offp" argument.
    (Logical change 1.45)

--- src/ia64/Gget_proc_name-ia64.c
+++ src/ia64/Gget_proc_name-ia64.c
@@ -1,5 +1,5 @@
 /* libunwind - a platform-independent unwind library
-   Copyright (C) 2001-2002 Hewlett-Packard Co
+   Copyright (C) 2001-2003 Hewlett-Packard Co
 	Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
 
 This file is part of libunwind.
@@ -26,10 +26,12 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 #include "unwind_i.h"
 
 int
-unw_get_proc_name (unw_cursor_t *cursor, char *buf, size_t buf_len)
+unw_get_proc_name (unw_cursor_t *cursor, char *buf, size_t buf_len,
+		   unw_word_t *offp)
 {
   struct cursor *c = (struct cursor *) cursor;
 
-  return unwi_get_proc_name (c->as, c->ip, c->as == unw_local_addr_space,
-			     buf, buf_len, c->as_arg);
+  return unwi_get_proc_name (c->as, c->ip,
+			     c->as == unw_local_addr_space,
+			     buf, buf_len, offp, c->as_arg);
 }
