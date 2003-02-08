52b17e2b3758f38271a71c1d7900cae3d3ce4937
mostang.com!davidm <mostang.com!davidm>
2003-02-08 10:10:59 +0000

    (unwi_put_dynamic_unwind_info): Don't call unwi_dyn_remote_put_unwind_info() when 	dealing with the local address space.
    (Logical change 1.45)

--- src/Gput_dynamic_unwind_info.c
+++ src/Gput_dynamic_unwind_info.c
@@ -32,7 +32,14 @@ unwi_put_dynamic_unwind_info (unw_addr_space_t as, unw_proc_info_t *pi,
   switch (pi->format)
     {
     case UNW_INFO_FORMAT_DYNAMIC:
+#ifndef UNW_LOCAL_ONLY
+# ifdef UNW_REMOTE_ONLY
       unwi_dyn_remote_put_unwind_info (as, pi, arg);
+# else
+      if (as != unw_local_addr_space)
+	unwi_dyn_remote_put_unwind_info (as, pi, arg);
+# endif
+#endif
       break;
 
     case UNW_INFO_FORMAT_TABLE:
