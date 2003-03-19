5fe7ac3ff5865c11773dedd9137a7a8fc06e614d
mostang.com!davidm <mostang.com!davidm>
2003-03-19 19:25:18 +0000

    (unw_set_caching_policy): Default to "global" policy if thread-local policy is 	not available.
    (Logical change 1.63)

--- src/ia64/set_caching_policy-ia64.c
+++ src/ia64/set_caching_policy-ia64.c
@@ -36,7 +36,7 @@ unw_set_caching_policy (unw_addr_space_t as, unw_caching_policy_t policy)
 
 #ifndef HAVE___THREAD
   if (policy == UNW_CACHE_PER_THREAD)
-    return -UNW_EINVAL;
+    policy = UNW_CACHE_GLOBAL;
 #endif
   as->caching_policy = policy;
 
