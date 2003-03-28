4f17bce117cf69d572523bbe3cfb2ee86efc0586
mostang.com!davidm <mostang.com!davidm>
2003-03-28 07:43:22 +0000

    Regenerate.
    (Logical change 1.70)

--- tests/Lia64-test-stack.c
+++ tests/Lia64-test-stack.c
@@ -1,2 +1,4 @@
+#ifndef UNW_REMOTE_ONLY
 #define UNW_LOCAL_ONLY
 #include "Gia64-test-stack.c"
+#endif
--- tests/Ltest-exc.c
+++ tests/Ltest-exc.c
@@ -1,2 +1,4 @@
+#ifndef UNW_REMOTE_ONLY
 #define UNW_LOCAL_ONLY
 #include "Gtest-exc.c"
+#endif
