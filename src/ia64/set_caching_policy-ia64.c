add6b939a04efad09c88d5f57242abf0cbfb486d
hp.com!davidm <hp.com!davidm>
2003-11-19 03:11:25 +0000

    (unw_set_caching_policy): Let ia64_init() clear unw.needs_initialization.
    (Logical change 1.111)

--- src/ia64/set_caching_policy-ia64.c
+++ src/ia64/set_caching_policy-ia64.c
@@ -29,10 +29,7 @@ int
 unw_set_caching_policy (unw_addr_space_t as, unw_caching_policy_t policy)
 {
   if (unw.needs_initialization)
-    {
-      unw.needs_initialization = 0;
-      ia64_init ();
-    }
+    ia64_init ();
 
 #ifndef HAVE___THREAD
   if (policy == UNW_CACHE_PER_THREAD)
