4d40b374b9f4c01795c7bb591ef53bea8b75ef51
mostang.com!davidm <mostang.com!davidm>
2003-12-05 06:42:28 +0000

    Lookup static proc-info via accessor's callback, not by directly calling tdep_find_proc_info().  The latter doesn't works in the REMOTE_ONLY case.
    (Logical change 1.138)

--- src/Gget_proc_info_by_ip.c
+++ src/Gget_proc_info_by_ip.c
@@ -32,10 +32,11 @@ int
 unw_get_proc_info_by_ip (unw_addr_space_t as, unw_word_t ip,
 			 unw_proc_info_t *pi, void *as_arg)
 {
+  unw_accessors_t *a = unw_get_accessors (as);
   int ret;
 
   ret = unwi_find_dynamic_proc_info (as, ip, pi, 0, as_arg);
   if (ret == -UNW_ENOINFO)
-    ret = tdep_find_proc_info (as, ip, pi, 0, as_arg);
+    ret = (*a->find_proc_info) (as, ip, pi, 0, as_arg);
   return ret;
 }
