6535a330cfc125a077f3a43216f317b6d929aabc
mostang.com!davidm <mostang.com!davidm>
2004-01-20 23:32:28 +0000

    Include "internal.h" and "tdep.h" instead of "unwind_i.h" to avoid name-space pollution when compiling with debugging enabled.  The name-space pollution came from the fact that functions won't be inlined in this case and that ended up generating dead code with references to non-existent functions.
    (Logical change 1.154)

--- src/ia64/flush_cache-ia64.c
+++ src/ia64/flush_cache-ia64.c
@@ -1,5 +1,5 @@
 /* libunwind - a platform-independent unwind library
-   Copyright (C) 2002-2003 Hewlett-Packard Co
+   Copyright (C) 2002-2004 Hewlett-Packard Co
 	Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
 
 This file is part of libunwind.
@@ -23,7 +23,8 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
-#include "unwind_i.h"
+#include "internal.h"
+#include "tdep.h"
 
 PROTECTED void
 unw_flush_cache (unw_addr_space_t as, unw_word_t lo, unw_word_t hi)
