3fd2b8adfad1cbd7b9524af041ac276163ac74ec
hp.com!davidm <hp.com!davidm>
2004-05-12 20:54:56 +0000

    (get_kernel_table): Change %Zu to %zu.
    (Logical change 1.236)

--- src/ia64/Gtables-ia64.c
+++ src/ia64/Gtables-ia64.c
@@ -398,7 +398,7 @@ get_kernel_table (unw_dyn_info_t *di)
   ktab = sos_alloc (size);
   if (!ktab)
     {
-      dprintf (__FILE__".%s: failed to allocate %Zu bytes",
+      dprintf (__FILE__".%s: failed to allocate %zu bytes",
 	       __FUNCTION__, size);
       return -UNW_ENOMEM;
     }
