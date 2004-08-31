e09e6ce1ad753017313688eafc466656c322f9d0
com[davidm]!masbock <com[davidm]!masbock>
2004-08-31 13:59:37 +0000

    x86-64: fix unw_get_proc_info()
    (Logical change 1.260)

--- src/x86_64/Gget_proc_info.c
+++ src/x86_64/Gget_proc_info.c
@@ -31,6 +31,11 @@ PROTECTED int
 unw_get_proc_info (unw_cursor_t *cursor, unw_proc_info_t *pi)
 {
   struct cursor *c = (struct cursor *) cursor;
+  int ret;
 
-  return dwarf_make_proc_info (&c->dwarf);
+  if (ret = dwarf_make_proc_info (&c->dwarf))
+    return ret;
+
+  *pi = c->dwarf.pi;
+  return 0;
 }
