83b02fa05b0b228a86e1055466447de312ff44a0
hp.com!davidm <hp.com!davidm>
2005-05-03 09:13:17 +0000

    Add missing include of <string.h> to get strerror() declared.
    (Logical change 1.290)

--- tests/ia64-test-setjmp.c
+++ tests/ia64-test-setjmp.c
@@ -34,6 +34,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 #include <setjmp.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/mman.h>
