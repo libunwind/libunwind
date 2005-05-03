e6446885f44866668ba7c5649c09386876e92a57
hp.com!davidm <hp.com!davidm>
2005-05-03 09:13:17 +0000

    Initial revision

--- src/ptrace/_UPT_elf.c
+++
@@ -1,5 +0,0 @@
-/* We need to get a separate copy of the ELF-code into
-   libunwind-ptrace since it cannot (and must not) have any ELF
-   dependencies on libunwind.  */
-#include "tdep.h"	/* get ELFCLASS defined */
-#include "../elf64.c"
