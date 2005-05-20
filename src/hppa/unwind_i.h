19df6612fa2f2be0a2828e46364b57e7a81f3079
hp.com!davidm <hp.com!davidm>
2005-05-20 09:48:08 +0000

    Include "libunwind_i.h" instead of "internal.h" and "tdep.h".
    (Logical change 1.294)

--- src/hppa/unwind_i.h
+++ src/hppa/unwind_i.h
@@ -1,5 +1,5 @@
 /* libunwind - a platform-independent unwind library
-   Copyright (C) 2004 Hewlett-Packard Co
+   Copyright (C) 2004-2005 Hewlett-Packard Co
 	Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
 
 This file is part of libunwind.
@@ -31,8 +31,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include <libunwind-hppa.h>
 
-#include "internal.h"
-#include "tdep.h"
+#include "libunwind_i.h"
 
 #define hppa_lock			UNW_OBJ(lock)
 #define hppa_local_resume		UNW_OBJ(local_resume)
