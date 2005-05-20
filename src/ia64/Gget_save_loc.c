8e6f3c84851c8efb66d3e153a22b8aff33f91da5
hp.com!davidm <hp.com!davidm>
2005-05-20 09:48:08 +0000

    Include "rse.h" instead of "ia64/rse.h".
    (Logical change 1.294)

--- src/ia64/Gget_save_loc.c
+++ src/ia64/Gget_save_loc.c
@@ -1,5 +1,5 @@
 /* libunwind - a platform-independent unwind library
-   Copyright (C) 2002-2003 Hewlett-Packard Co
+   Copyright (C) 2002-2003, 2005 Hewlett-Packard Co
 	Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
 
 This file is part of libunwind.
@@ -25,7 +25,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include <assert.h>
 
-#include "ia64/rse.h"
+#include "rse.h"
 
 #include "offsets.h"
 #include "regs.h"
