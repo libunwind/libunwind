c027d9838698e187aeebfbf552674886ce3169cf
hp.com!davidm <hp.com!davidm>
2005-05-20 09:48:08 +0000

    Don't include headers already included via unwind_i.h.
    (Logical change 1.294)

--- src/ia64/Gparser.c
+++ src/ia64/Gparser.c
@@ -23,11 +23,6 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "unwind_i.h"
 
 /* forward declaration: */
