32e2187fab8d7a0adaa099b7bdaefca50628de88
Jose Flavio Aguilar Paulino <jflavio@br.ibm.com>
2007-08-27 09:11:37 -0600

    [PPC64] Get "make check" to work on PPC64 Linux.  Add a README entry 	describing the expected results.

--- src/ptrace/_UPT_find_proc_info.c
+++ src/ptrace/_UPT_find_proc_info.c
@@ -145,7 +145,7 @@ _UPTi_find_unwind_table (struct UPT_info *ui, unw_addr_space_t as,
   return &ui->di_cache;
 }
 
-#elif UNW_TARGET_X86 || UNW_TARGET_X86_64 || UNW_TARGET_HPPA
+#elif UNW_TARGET_X86 || UNW_TARGET_X86_64 || UNW_TARGET_HPPA || UNW_TARGET_PPC64
 
 #include "dwarf-eh.h"
 #include "dwarf_i.h"
