da357e3c2e34c4d9d51e22d778a6dcea6eccef56
Konstantin Belousov <kostik@pooma.home>
2010-03-08 00:20:23 +0200

    Revert "sys/types.h"
    This reverts commit 5976f1a5b33ba1af3e7dbc663ffbebca3b944024.

--- tests/Lperf-simple.c
+++ tests/Lperf-simple.c
@@ -1,11 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef HAVE_SYS_TYPES_H
-# include <sys/types.h>
-#endif
-
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
 #if !defined(UNW_REMOTE_ONLY)
