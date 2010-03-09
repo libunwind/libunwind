bb41eba56c44306e5bc2dcf8eeba574bd5a3702f
Konstantin Belousov <kostikbel@gmail.com>
2010-03-09 18:01:08 +0200

    FreeBSD/i386 port. get/setcontext need further work

--- src/x86/Gcreate_addr_space.c
+++ src/x86/Gcreate_addr_space.c
@@ -27,6 +27,10 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include "unwind_i.h"
 
+#if defined(_LITTLE_ENDIAN) && !defined(__LITTLE_ENDIAN)
+#define __LITTLE_ENDIAN _LITTLE_ENDIAN
+#endif
+
 PROTECTED unw_addr_space_t
 unw_create_addr_space (unw_accessors_t *a, int byte_order)
 {
