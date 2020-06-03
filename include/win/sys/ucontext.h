9165d2a150d707d3037c2045f2cdc0fabd5fee98
Steve MacLean <Steve.MacLean@microsoft.com>
2020-06-03 14:41:59 -0400

    Fix Windows abstraction SIZEOF_*
    Use actual sizes from current kernels for the Windows abstraction layer

--- include/win/sys/ucontext.h
+++ include/win/sys/ucontext.h
@@ -9,9 +9,23 @@
 #ifdef _MSC_VER // Only for cross compilation to windows
 #include <inttypes.h>
 
+#if defined(__linux__) && defined(__x86_64__)
+#  define SIZEOF_UCONTEXT 936
+#elif defined(__linux__) && defined(__aarch64__)
+#  define SIZEOF_UCONTEXT 4560
+#elif defined(__linux__) && defined(__arm__)
+#  define SIZEOF_UCONTEXT 744
+#elif !defined(SIZEOF_UCONTEXT)
+  // It is not clear whether the sizeof(ucontext_t) is important
+  // While compiling on Windows the members are not referenced...
+  // However the size maybe important during a case or a memcpy
+  // Barring a full audit it could be important so require the size to be defined
+#  error SIZEOF_UCONTEXT is unknown for this target
+#endif
+
 typedef struct ucontext
 {
-    uint8_t content[SIZEOF_UCONTENT];
+    uint8_t content[SIZEOF_UCONTEXT];
 } ucontext_t;
 
 #ifdef __aarch64__
