af5e6d3ab3d8f8ba8cc955b6ed70712e2165c9ad
Adeel <3840695+am11@users.noreply.github.com>
2021-11-27 08:43:32 +0200

    Add more POSIX polyfills for nix cross-build on win

--- src/win/pal-single-threaded.c
+++ src/win/pal-single-threaded.c
@@ -13,10 +13,10 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include "libunwind_i.h"
 #include "compiler.h"
-
 
 int getpagesize(void)
 {
@@ -111,4 +111,14 @@ int close(int fd)
 unw_accessors_t * unw_get_accessors_int (unw_addr_space_t as)
 {
     return unw_get_accessors(as);
+}
+
+int stat(const char *path, struct stat *buf)
+{
+    return 0;
+}
+
+int fstat(int fd, struct stat *buf)
+{
+    return 0;
 }
