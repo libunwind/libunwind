d3583edb22b84cf8fe46c0d6b2e5454b453f4acc
mikael <mikael@FreeBSD.org>
2022-03-23 10:07:49 +0100

    Fix for FreeBSD aarch64

--- include/libunwind-aarch64.h
+++ include/libunwind-aarch64.h
@@ -225,15 +225,24 @@ typedef struct
 #else
 /* On AArch64, we can directly use ucontext_t as the unwind context.  */
 typedef ucontext_t unw_tdep_context_t;
+#if defined(__FreeBSD__)
+typedef ucontext_t unw_fpsimd_context_t;
+#endif
 #endif
 
 
 #include "libunwind-common.h"
 #include "libunwind-dynamic.h"
 
+#if defined(__FreeBSD__)
+#define UNW_BASE register uint64_t unw_base __asm__ ("x0") = (uint64_t) unw_ctx->uc_mcontext.mc_gpregs.gp_x[0];
+#else
+#define UNW_BASE register uint64_t unw_base __asm__ ("x0") = (uint64_t) unw_ctx->uc_mcontext.regs;
+#endif
+
 #define unw_tdep_getcontext(uc) ({					\
   unw_tdep_context_t *unw_ctx = (uc);					\
-  register uint64_t unw_base __asm__ ("x0") = (uint64_t) unw_ctx->uc_mcontext.regs; \
+  UNW_BASE \
   __asm__ __volatile__ (					        \
      "stp x0, x1, [%[base], #0]\n" \
      "stp x2, x3, [%[base], #16]\n" \
--- src/aarch64/Ginit.c
+++ src/aarch64/Ginit.c
@@ -47,9 +47,17 @@ static inline void *
 uc_addr (unw_tdep_context_t *uc, int reg)
 {
   if (reg >= UNW_AARCH64_X0 && reg < UNW_AARCH64_V0)
+#if defined(__FreeBSD__)
+    return &uc->uc_mcontext.mc_gpregs.gp_x[reg];
+#else
     return &uc->uc_mcontext.regs[reg];
+#endif
   else if (reg >= UNW_AARCH64_V0 && reg <= UNW_AARCH64_V31)
+#if defined(__FreeBSD__)
+    return &GET_FPCTX(uc)->uc_mcontext.mc_fpregs.fp_q[reg - UNW_AARCH64_V0];
+#else
     return &GET_FPCTX(uc)->vregs[reg - UNW_AARCH64_V0];
+#endif
   else
     return NULL;
 }
