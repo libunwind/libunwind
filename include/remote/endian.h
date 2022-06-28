cfcf3d193a4425f58ed9556ec54b6d7ab0557330
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2022-06-29 02:43:45 +0300

    Add remote unwinding support for macOS (#365)
    * Add remote unwinding support for macOS
    * Fix broken Win build by bumping minimum language requirement to C11
    * Update license headers
    * Rename remote_unwind to remote
    * Revert Gparser.c

--- include/remote/endian.h
+++ include/remote/endian.h
@@ -1,16 +1,12 @@
 // This is an incomplete & imprecice implementation of the
 // standard file by the same name
 
-
-// Since this is only intended for VC++ compilers
-// use #pragma once instead of guard macros
-#pragma once
-
-#ifdef _MSC_VER // Only for cross compilation to windows
+#ifndef ENDIAN_H
+#define ENDIAN_H
 
 #define __LITTLE_ENDIAN        1234
 #define __BIG_ENDIAN           4321
 
 #define __BYTE_ORDER __LITTLE_ENDIAN
 
-#endif // _MSC_VER
+#endif // ENDIAN_H
