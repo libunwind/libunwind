40a8a0ff9fe2d89c1aaa14eb9e8b212e7b2e2f40
Dmitry Chagin <dchagin@FreeBSD.org>
2023-05-21 19:24:27 +0300

    Fix bt test on FreeBSD/aarch64 where SIGBUS instead of SIGSEGV raised.

--- tests/Gtest-bt.c
+++ tests/Gtest-bt.c
@@ -254,6 +254,8 @@ main (int argc, char **argv UNUSED)
   act.sa_handler = (void (*)(int)) segv_handler;
   if (sigaction (SIGSEGV, &act, NULL) < 0)
     panic ("sigaction: %s\n", strerror (errno));
+  if (sigaction (SIGBUS, &act, NULL) < 0)
+    panic ("sigaction: %s\n", strerror (errno));
 
   if (sigsetjmp (env, 1) == 0)
   {
