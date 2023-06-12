9d451a8f250c79f9d88061b50b50c3a69e228d40
Dmitry Chagin <dchagin@FreeBSD.org>
2023-06-12 18:33:27 +0300

    Reduce warnings on FreeBSD/x86_64, unhide tdep_stash_frame under __linux__ as it's should be OS independent.

--- include/tdep-x86_64/libunwind_i.h
+++ include/tdep-x86_64/libunwind_i.h
@@ -291,9 +291,9 @@ extern void tdep_fetch_frame (struct dwarf_cursor *c, unw_word_t ip,
 extern int tdep_cache_frame (struct dwarf_cursor *c);
 extern void tdep_reuse_frame (struct dwarf_cursor *c,
                               int frame);
+#endif
 extern void tdep_stash_frame (struct dwarf_cursor *c,
                               struct dwarf_reg_state *rs);
-#endif
 
 extern int tdep_getcontext_trace (unw_tdep_context_t *);
 extern int tdep_trace (unw_cursor_t *cursor, void **addresses, int *n);
