aa8485155c9d7fa6fc08787bed6ce7e0271295c9
Dmitry Chagin <dchagin@FreeBSD.org>
2023-06-14 01:11:50 +0300

    Fix siglongjmp for FreeBSD.
    It should be a pointer to a sigset_t, not its contents.

--- src/setjmp/siglongjmp.c
+++ src/setjmp/siglongjmp.c
@@ -108,7 +108,7 @@ siglongjmp (sigjmp_buf env, int val)
                     && unw_set_reg (&c, UNW_REG_EH + 3, wp[JB_MASK + 1]) < 0))
               abort ();
 #elif defined(__FreeBSD__)
-          if (unw_set_reg (&c, UNW_REG_EH + 2, wp[JB_MASK]) < 0)
+          if (unw_set_reg (&c, UNW_REG_EH + 2, (unw_word_t)&wp[JB_MASK]) < 0)
               abort();
 #else
 #error Port me
