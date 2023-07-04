72690dd9d01c27d9a4759039caa6de7e050e5cef
Kent Cheung <kent.cheung@linaro.org>
2023-07-04 16:21:28 +0100

    Added mutex for prepending/traversing cached eh_frame_hdr list.

--- src/dwarf/Gfind_proc_info-lsb.c
+++ src/dwarf/Gfind_proc_info-lsb.c
@@ -525,41 +525,53 @@ typedef struct eh_frame_list_entry
   }
 eh_frame_list_entry_t;
 
-/* Prepend entry to singly linked list head.  */
+static define_lock (eh_frame_list_lock);
+static eh_frame_list_entry_t *eh_frame_list_head;
+
+/* Prepend entry to singly-linked list head.  */
 static void
-eh_frame_list_prepend (eh_frame_list_entry_t **head, eh_frame_list_entry_t *entry)
+eh_frame_list_prepend (eh_frame_list_entry_t *entry)
 {
-  entry->next = *head;
-  *head = entry;
+  mutex_lock (&eh_frame_list_lock);
+  {
+    entry->next = eh_frame_list_head;
+    eh_frame_list_head = entry;
+  }
+  mutex_unlock (&eh_frame_list_lock);
 }
 
 /* Search singly linked list head for item with given filename.  */
 static eh_frame_list_entry_t*
-eh_frame_list_search (eh_frame_list_entry_t *head, const char *filename)
+eh_frame_list_search (const char *filename)
 {
-  eh_frame_list_entry_t *entry = head;
-  while (entry)
-    {
-      if (strcmp (entry->filename, filename) == 0)
-        {
-          Debug (6, "cache: found matching %s at 0x%lx\n",
-                 entry->filename,
-                 entry->eh_frame);
-          return entry;
-        }
-      Debug (6, "cache: %s at 0x%lx did not match\n",
-             entry->filename,
-             entry->eh_frame);
-      entry = entry->next;
-    }
+  mutex_lock (&eh_frame_list_lock);
+  {
+    eh_frame_list_entry_t *entry = eh_frame_list_head;
+    while (entry)
+      {
+        if (strcmp (entry->filename, filename) == 0)
+          {
+            Debug (6, "cache: found matching %s at 0x%lx\n",
+                   entry->filename,
+                   entry->eh_frame);
+
+            mutex_unlock (&eh_frame_list_lock);
+            return entry;
+          }
+        Debug (6, "cache: %s at 0x%lx did not match\n",
+               entry->filename,
+               entry->eh_frame);
+        entry = entry->next;
+      }
+  }
+  mutex_unlock (&eh_frame_list_lock);
+
   Debug (6, "cache: %s not in cache\n", filename);
   return NULL;
 }
 
-static eh_frame_list_entry_t *eh_frame_list;
-
 static Elf_W (Addr)
-dwarf_find_eh_frame_section(struct dl_phdr_info *info)
+find_eh_frame_section (struct dl_phdr_info *info)
 {
   int rc;
   struct elf_image ei;
@@ -576,7 +588,7 @@ dwarf_find_eh_frame_section(struct dl_phdr_info *info)
     }
 
   /* Check if cached synthetic eh_frame header exists for this file.  */
-  eh_frame_list_entry = eh_frame_list_search (eh_frame_list, file);
+  eh_frame_list_entry = eh_frame_list_search (file);
   if (eh_frame_list_entry)
     {
       return eh_frame_list_entry->eh_frame;
@@ -601,7 +613,8 @@ dwarf_find_eh_frame_section(struct dl_phdr_info *info)
   GET_MEMORY (eh_frame_list_entry, sizeof (eh_frame_list_entry_t) + strlen (file) + 1);
   strcpy (eh_frame_list_entry->filename, file);
   eh_frame_list_entry->eh_frame = eh_frame;
-  eh_frame_list_prepend (&eh_frame_list, eh_frame_list_entry);
+  eh_frame_list_entry->next = NULL;
+  eh_frame_list_prepend (eh_frame_list_entry);
 
 out:
   mi_munmap (ei.image, ei.size);
@@ -694,7 +707,7 @@ dwarf_callback (struct dl_phdr_info *info, size_t size, void *ptr)
     {
       Elf_W (Addr) eh_frame;
       Debug (1, "no .eh_frame_hdr section found\n");
-      eh_frame = dwarf_find_eh_frame_section (info);
+      eh_frame = find_eh_frame_section (info);
       if (eh_frame)
         {
           Debug (1, "using synthetic .eh_frame_hdr section for %s\n",
