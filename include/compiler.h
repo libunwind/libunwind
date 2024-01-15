48c3f2da887ad4e7eb90bd01a8504532739c8d03
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-01-15 05:44:09 +0200

    Check existence of attribute

--- include/compiler.h
+++ include/compiler.h
@@ -31,6 +31,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 #define COMPILER_H
 
 #ifdef __GNUC__
+#ifndef __has_attribute
+#  define __has_attribute(x) (0)
+#endif
 # define CONST_ATTR     __attribute__((__const__))
 # define UNUSED         __attribute__((unused))
 # define NOINLINE       __attribute__((noinline))
@@ -40,7 +43,11 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 # if (__GNUC__ > 3) || (__GNUC__ == 3 && __GNUC_MINOR__ > 2)
 #  define ALWAYS_INLINE inline __attribute__((always_inline))
 #  define HIDDEN        __attribute__((visibility ("hidden")))
-#  define FALLTHROUGH   __attribute__((fallthrough))
+#  if __has_attribute(fallthrough)
+#    define FALLTHROUGH __attribute__((fallthrough))
+#  else
+#    define FALLTHROUGH
+#  endif
 # else
 #  define ALWAYS_INLINE
 #  define HIDDEN
