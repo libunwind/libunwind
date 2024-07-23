e3f88e8e4b9d2c2df4932a759c269e69dcc3bb97
gdh1995 <gdh1995@qq.com>
2024-07-24 01:06:25 +0800

    Fix all occurrences of returning -UNW_ESTOPUNWIND

--- src/arm/Gtrace.c
+++ src/arm/Gtrace.c
@@ -533,8 +533,9 @@ tdep_trace (unw_cursor_t *cursor, void **buffer, int *size)
       /* We cannot trace through this frame, give up and tell the
           caller we had to stop.  Data collected so far may still be
           useful to the caller, so let it know how far we got.  */
-      ret = -UNW_ESTOPUNWIND;
-      break;
+      Debug (1, "returning UNW_ESTOPUNWIND, depth %d\n", depth);
+      *size = depth;
+      return -UNW_ESTOPUNWIND;
     }
 
     Debug (4, "new cfa 0x%x pc 0x%x sp 0x%x r7 0x%x\n",
