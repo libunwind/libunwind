b33b3cd76cfc977c9fcc8d292260a3b36a7f4ac5
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2024-10-06 02:40:58 +0800

    Fix build on FreeBSD (32-bit arm)
    Reference:	https://cgit.freebsd.org/ports/commit/?id=663a5a08daebb7563af762e032c9776d4b2846aa

--- src/arm/Gtrace.c
+++ src/arm/Gtrace.c
@@ -514,7 +514,9 @@ tdep_trace (unw_cursor_t *cursor, void **buffer, int *size)
       if (likely(ret >= 0))
         ACCESS_MEM_FAST(ret, c->validate, d, cfa + LINUX_SC_LR_OFF, lr);
 #elif defined(__FreeBSD__)
-      #error implement UNW_ARM_FRAME_SIGRETURN on FreeBSD
+      Dprintf ("%s: implement me\n", __FUNCTION__);
+      ret = -UNW_ESTOPUNWIND;
+      break;
 #endif
 
       /* Resume stack at signal restoration point. The stack is not
